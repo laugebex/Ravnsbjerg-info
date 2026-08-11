4a68b24489a3fc3201722c48770b392fa21eb081
laugebex <laugebex@gmail.com>
2026-08-11 22:03:54 +0200

    Indlæs UX-styles og opdater metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,12 @@
 import './globals.css'
 import './review.css'
+import './ux.css'
 import {Header} from '../components/Header'
 import {Footer} from '../components/Footer'
 
 export const metadata={
   title:'Grundejerforeningen Ravnsbjerg',
-  description:'Nyt fra Ravnsbjerg, Lunden, vedtægter, bestyrelse og information om området.',
+  description:'Vedtægter, referater, Lunden, bestyrelsen og praktisk information om Ravnsbjerg i Gjellerup.',
   metadataBase:new URL('https://ravnsbjerg.info')
 }
 
