58af5eb34f402f5d085780467dc0f13a1dc7b001
laugebex <laugebex@gmail.com>
2026-08-12 12:51:46 +0200

    Add social metadata to bylaws

--- app/vedtaegter/layout.jsx
+++ app/vedtaegter/layout.jsx
@@ -1,3 +1,7 @@
 import {BylawsTools} from '../../components/BylawsTools'
 
+const title='Vedtægter | Grundejerforeningen Ravnsbjerg'
+const description='Gældende vedtægter for Grundejerforeningen Ravnsbjerg.'
+export const metadata={title,description,openGraph:{title,description,url:'/vedtaegter'}}
+
 export default function VedtaegterLayout({children}){return <><BylawsTools/>{children}</>}
