e281e5a7c910ddfd3419d88edb5887489e40a9a3
laugebex <laugebex@gmail.com>
2026-08-23 15:42:12 +0200

    Enable Vercel Web Analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import './ux.css'
 import './home.css'
 import {Header} from '../components/Header'
 import {Footer} from '../components/Footer'
+import {Analytics} from '@vercel/analytics/next'
 
 export const metadata={
   title:'Grundejerforeningen Ravnsbjerg',
@@ -20,5 +21,5 @@ export const metadata={
 }
 
 export default function RootLayout({children}){
-  return <html lang="da"><body><a className="skipLink" href="#main-content">Spring til indhold</a><Header/><div id="main-content" tabIndex="-1">{children}</div><Footer/></body></html>
+  return <html lang="da"><body><a className="skipLink" href="#main-content">Spring til indhold</a><Header/><div id="main-content" tabIndex="-1">{children}</div><Footer/><Analytics/></body></html>
 }
