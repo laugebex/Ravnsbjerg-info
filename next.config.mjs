c6e1d0f65c5a40a550fad2f0bf2b92c34e266dd7
laugebex <laugebex@gmail.com>
2026-08-23 19:45:37 +0200

    Fix Lunden page redirect

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig={
   async redirects(){return[
     {source:'/bestyrelsesmedlemmer',destination:'/#bestyrelsen',permanent:true},
-    {source:'/lunden',destination:'/#lunden',permanent:true},
     {source:'/hjertestarter',destination:'/#hjertestarter',permanent:true},
     {source:'/generalforsamlingen',destination:'/generalforsamling',permanent:true},
     {source:'/om-grundejerforeningen',destination:'/',permanent:true}
