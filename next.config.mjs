508222e99c96437a33011016dfe89a8401dc6bc4
laugebex <laugebex@gmail.com>
2026-09-06 14:10:17 +0200

    Send gammel Hall’n-side til Hall’ns egen side

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,8 @@ const nextConfig={
     {source:'/bestyrelsesmedlemmer',destination:'/#bestyrelsen',permanent:true},
     {source:'/hjertestarter',destination:'/#hjertestarter',permanent:true},
     {source:'/generalforsamlingen',destination:'/generalforsamling',permanent:true},
-    {source:'/om-grundejerforeningen',destination:'/',permanent:true}
+    {source:'/om-grundejerforeningen',destination:'/',permanent:true},
+    {source:'/sport',destination:'https://halln.dk/sport-fritid/',permanent:true}
   ]}
 }
 
